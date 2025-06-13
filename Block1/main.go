25087c9b4e54ea0c77fd044b71d5d3a8a2c75e2f
Aleksey Pudovkin <freem4n382@yandex.ru>
2025-06-13 12:15:36 +0300

    ✅ Блок 1: Ввод-вывод и арифметика (без условий)

--- Block1/main.go
+++ Block1/main.go
@@ -10,23 +10,28 @@ func main() {
 	timeConverter(135)
 }
 
+//1. 🧮 Сложение двух чисел
 func sum(a int, b int) {
 	fmt.Println(a + b)
 }
 
+//2. ➗ Деление на два
 func divide(a float64, b float64) {
 	fmt.Println(a / b)
 }
 
+//3. 📏 Перевод сантиметров в метры
 func smToMeters(sm float64) {
 	fmt.Printf("Вывод: %g м\n", (sm / 100))
 }
 
+//4. 💸 Расчёт стоимости
 func costCalculation(price float64, count int) {
 	fullPrice := price * float64(count)
 	fmt.Println(fullPrice)
 }
 
+//5. 🕐 Конвертер времени
 func timeConverter(minutes int) {
 	fmt.Printf("Вывод: %d часа %d минут!\n", (minutes / 60), (minutes % 60))
 }
