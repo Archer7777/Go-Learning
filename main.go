8be68a38d7ad7242df52ac72909f876d21140f2d
Aleksey Pudovkin <freem4n382@yandex.ru>
2025-06-13 11:44:23 +0300

    IO + arithmetics block

--- main.go
+++ main.go
@@ -1,15 +1,32 @@
 package main
 
-import (
-	"fmt"
-	"math/rand"
-	"time"
-)
+import "fmt"
 
 func main() {
-	for {
-		nums := rand.Intn(1_000_000)
-		fmt.Printf("S%06d\n", nums)
-		time.Sleep(500 * time.Millisecond)
-	}
+	sum(2, 5)
+	divide(10, 2)
+	smToMeters(250)
+	costCalculation(50, 3)
+	timeConverter(135)
+}
+
+func sum(a int, b int) {
+	fmt.Println(a + b)
+}
+
+func divide(a float64, b float64) {
+	fmt.Println(a / b)
+}
+
+func smToMeters(sm float64) {
+	fmt.Printf("Вывод: %g м\n", (sm / 100))
+}
+
+func costCalculation(price float64, count int) {
+	fullPrice := price * float64(count)
+	fmt.Println(fullPrice)
+}
+
+func timeConverter(minutes int) {
+	fmt.Printf("Вывод: %d часа %d минут\n", (minutes / 60), (minutes % 60))
 }
